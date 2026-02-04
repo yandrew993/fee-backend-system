0e1aabf9a90859a4ae1212a1852380ce9abbe74a
Andrew Young <yandrew993@gmail.com>
2026-02-04 02:33:44 -0800

    Allowed CORS

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5173",
+  "https://surewayfeemanagement.surewaygroupofschools.org",
 ].filter(Boolean);
 
 app.use(
